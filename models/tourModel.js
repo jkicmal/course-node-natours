c527d686d35e50a2b18a7e703689dc5455765364
jkicmal <jakub.kicmal@gmail.com>
2019-08-22 21:00:45 +0200

    add validators to tourModel, add model middlewares

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,59 +1,141 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name.'],
-    unique: true,
-    trim: true
+const tourSchema = new mongoose.Schema(
+  {
+    // Schema definition
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name.'],
+      unique: true,
+      trim: true,
+      // This is a validator, if requirement is not met it returs an error
+      // String validators
+      maxlength: [
+        40,
+        'A tour name must have less or equal than 40 characters.'
+      ],
+      minlength: [5, 'A tour name must have more than 5 characters']
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration.']
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size.']
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty.'],
+      trim: true,
+      // String validator
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium or difficult'
+      }
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      // Number validators
+      min: [1, 'Rating must be above 1.0'], // Also works for dates
+      max: [5, 'Rating must be below 5.0']
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price.']
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a summary.']
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description.']
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false
+    },
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration.']
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size.']
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty.'],
-    trim: true
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price.']
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a summary.']
-  },
-  description: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description.']
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image']
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false
-  },
-  startDates: [Date]
+  // Schema options object
+  {
+    toJSON: { virtuals: true }, // Use virtuals when outputing as JSON
+    toObject: { virtuals: true } // Use virtuals when outputing as Object
+  }
+);
+
+// Define virtual property (does not exist in db, derived from existing data)
+// Has to be a regular function because we need 'this' keyword
+// Virtual properties cannot be used in queries
+tourSchema.virtual('durationWeeks').get(function() {
+  return this.duration / 7;
+});
+
+// Document middleware
+// Runs before .save() and .create()
+// 'save' is a 'hook'
+tourSchema.pre('save', function(next) {
+  // Slug property must be defined on schema
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function(next) {
+//   console.log('Will save document...');
+//   next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+// Query middleware
+// Works only for .find(), not .findOne()
+// /^find/ - all strings starting with 'find'
+tourSchema.pre(/^find/, function(next) {
+  // 'this' keyword points to query
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now(); // Remember that query is just a normal object
+
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} miliseconds.`);
+  // console.log(docs);
+  next();
+});
+
+// Aggregation middleware
+// 'this' points to the current aggregation
+tourSchema.pre('aggregate', function(next) {
+  // Add another $match at the end of pipeline array
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  // console.log(this.pipeline());
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
